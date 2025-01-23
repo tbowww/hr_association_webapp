ede5a175a8c964a2c7d5f70e684385de645871e3
Thibault Etienne <Thibault.Etienne@arhs-digital.com>
2025-01-23 16:57:07 +0100

    create the controller and view

---
+++ src/main/java/com/openclassrooms/webapp/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.openclassrooms.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+}
